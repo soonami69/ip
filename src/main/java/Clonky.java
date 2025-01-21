0bb020f2f60623894de1b0b64f647dec34dec71d
soonami69 <timothysoonwf@gmail.com>
2025-01-21 21:19:58 +0800

    Level 0

---
+++ src/main/java/Clonky.java
@@ -0,0 +1,15 @@
+public class Clonky {
+    public static void main(String[] args) {
+        String logo = """
+        _________ .__                 __           
+        \\_   ___ \\|  |   ____   ____ |  | _____.__.
+        /    \\  \\/|  |  /  _ \\ /    \\|  |/ <   |  |
+        \\     \\___|  |_(  <_> )   |  \\    < \\___  |
+         \\______  /____/\\____/|___|  /__|_ \\/ ____|
+                \\/                 \\/     \\/\\/     
+        """;
+
+
+        System.out.println("Hello! I'm \n" + logo + "\nWhat can I do for you?");
+    }
+}
