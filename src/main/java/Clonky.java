e1c183c412b53ad48d4badc5e4f605cc26f6cca8
soonami69 <timothysoonwf@gmail.com>
2025-01-21 21:37:03 +0800

    Level 2

--- src/main/java/Clonky.java
+++ src/main/java/Clonky.java
@@ -12,20 +12,34 @@ public class Clonky {
         """;
 
 
-        System.out.println("Hello! I'm \n" + logo + "\nWhat can I do for you?");
+        System.out.println("Hello! I'm \n" + logo + "\nWhat would you like me to store?");
         Scanner scanner = new Scanner(System.in);
+        String[] items = new String[100];
+        int count = 0;
+        System.out.println("____________________________________________________________");
         while (true) {
             System.out.print("You: ");
             String userInput = scanner.nextLine().trim();
-
+            System.out.println("____________________________________________________________");
             // user exiting
             if (userInput.equalsIgnoreCase("bye")) {
                 System.out.println("Clonky: Get out!");
+                System.out.println("____________________________________________________________");
                 break;
             }
-
-            System.out.println("____________________________________________________________");
-            System.out.println("Clonky: " + userInput);
+            if (userInput.equalsIgnoreCase("list")) {
+                if (count == 0) {
+                    System.out.println("No items!");
+                } else {
+                    for (int i = 0; i < count; i++) {
+                        System.out.println(items[i]);
+                    }
+                }
+            } else {
+                System.out.println("Clonky: " + userInput);
+                items[count] = userInput;
+                count += 1;
+            }
             System.out.println("____________________________________________________________");
         }
     }
