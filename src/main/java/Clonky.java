27f87d737d15e6318d69f5c53cdf44ae5ad9ed8a
soonami69 <timothysoonwf@gmail.com>
2025-01-21 22:06:06 +0800

    Level 3

--- src/main/java/Clonky.java
+++ src/main/java/Clonky.java
@@ -14,30 +14,58 @@ public class Clonky {
 
         System.out.println("Hello! I'm \n" + logo + "\nWhat would you like me to store?");
         Scanner scanner = new Scanner(System.in);
-        String[] items = new String[100];
+        Task[] items = new Task[100];
         int count = 0;
         System.out.println("____________________________________________________________");
         while (true) {
             System.out.print("You: ");
-            String userInput = scanner.nextLine().trim();
+            String userInput = scanner.next();
             System.out.println("____________________________________________________________");
             // user exiting
             if (userInput.equalsIgnoreCase("bye")) {
                 System.out.println("Clonky: Get out!");
-                System.out.println("____________________________________________________________");
                 break;
             }
-            if (userInput.equalsIgnoreCase("list")) {
+            if (userInput.equalsIgnoreCase("mark")) {
+                // scan in next integer and mark as completed
+                int idx = scanner.nextInt() - 1;
+                if (idx < 0 || idx > 100 || idx >= count) {
+                    System.out.println("Invalid index!");
+                    continue;
+                }
+                if (items[idx].isDone) {
+                    System.out.println("Task already complete!");
+                } else {
+                    items[idx].markAsDone();
+                    System.out.println("Task " + items[idx].description +
+                            " successfully marked.");
+                }
+            } else if (userInput.equalsIgnoreCase("unmark")) {
+                // scan in next integer and mark as uncompleted
+                int idx = scanner.nextInt() - 1;
+                if (idx < 0 || idx > 100 || idx >= count) {
+                    System.out.println("Invalid index!");
+                    continue;
+                }
+                if (!items[idx].isDone) {
+                    System.out.println("Task already incomplete!");
+                } else {
+                    items[idx].markAsUndone();
+                    System.out.println("Task " + items[idx].description +
+                            " successfully unmarked.");
+                }
+            } else if (userInput.equalsIgnoreCase("list")) {
+                System.out.println("Here is your list of tasks:");
                 if (count == 0) {
                     System.out.println("No items!");
                 } else {
                     for (int i = 0; i < count; i++) {
-                        System.out.println(items[i]);
+                        System.out.println((i + 1) + ". " + items[i].toString());
                     }
                 }
             } else {
                 System.out.println("Clonky: " + userInput);
-                items[count] = userInput;
+                items[count] = new Task(userInput);
                 count += 1;
             }
             System.out.println("____________________________________________________________");
